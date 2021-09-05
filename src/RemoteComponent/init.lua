7028be7658f7ff61d50ee1ac1148e9432f501fac
EncodedVenom <EncodedVenomRBLX@gmail.com>
2021-09-05 13:02:46 -0400

    Separate Client table to fix issues once and for all.

--- src/RemoteComponent/init.lua
+++ src/RemoteComponent/init.lua
@@ -215,16 +215,6 @@ function Component.new(tag, class, renderPriority, requireComponents)
 	self._janitor:Add(observeJanitor)
 	self._janitor:Add(self._lifecycleJanitor)
 
-	if self._class.Client and IS_SERVER then
-		self._typesOfObjectsToAdd = {}
-		for name,object in pairs(self._class.Client) do
-			if (type(object)=="function") then
-				self._typesOfObjectsToAdd[name] = "function"
-			elseif (RemoteSignal.Is(object)) then
-				self._typesOfObjectsToAdd[name] = "signal"
-			end
-		end
-	end
 
 
 	local function ObserveTag()
@@ -404,16 +394,17 @@ function Component:_instanceAdded(instance)
 	if (IS_SERVER) then
 		instance:SetAttribute(ATTRIBUTE_ID_NAME, id)
 		if obj.Client then
+			obj.Client = TableUtil.Copy(obj.Client) -- Separate the class Client folder from the object Client folder.
 			obj._serverComm = Comm.Server.ForParent(instance, self._tag, self._janitor)
 			obj.Client.Server = obj
-			for name,object in pairs(self._typesOfObjectsToAdd) do
-				if (object=="function") then -- Logic to determine what type of object is what is done in the .new constructor. Fixes bug with REs not being properly created.
+			for name,object in pairs(self._class.Client) do
+				if (type(object)=="function") then
 					obj._serverComm:BindFunction(name, function(Player, ...)
 						local args = {...} -- This is weird. Adding for backwards compatibility.
 						table.remove(args, 1)
 						return obj.Client[name](obj.Client, Player, table.unpack(args))
 					end)
-				elseif (object=="signal") then
+				elseif (RemoteSignal.Is(object)) then
 					obj.Client[name] = obj._serverComm:CreateSignal(name)
 				end
 			end
